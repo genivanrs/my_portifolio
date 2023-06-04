cea4c412014c33bc2088b20689d161da1fcb04c8
Genivan Rocha dos Santos <85045540+genivanrs@users.noreply.github.com>
2023-06-04 14:46:57 -0300

    Add files via upload

---
+++ My_Portifolio/js/scripts.js
@@ -0,0 +1,8 @@
+ src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js">
+ $(function(){                       
+    $('#svg').load("svg_dots/svg_dots.html"); /*chama o codigo svg*/ 
+    $('#footer').load("footer/footer.html"); /*chama o codigo footer*/ 
+    $('#nav').load("nav/nav.html"); /*chama o codigo*/                                       
+    });    
+                                                               
+        
